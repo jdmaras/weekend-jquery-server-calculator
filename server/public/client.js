025574cf1560a31cb791cefc1ec018046564a334
John Maras <sportsballenthusiast@gmail.com>
2022-05-20 17:01:30 -0500

    put in the buttons and they click

--- server/public/client.js
+++ server/public/client.js
@@ -2,4 +2,14 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log(`I'm ready - Sponge Bob`);
+  $("#additionButton").on("click", onSubmit);
+  $("#subtractionButton").on("click", onSubmit);
+  $("#multiplicationButton").on("click", onSubmit);
+  $("#divideButton").on("click", onSubmit);
+  $("#clearButton").on("click", onSubmit);
+  $("#equalButton").on("click", onSubmit);
+}
+
+function onSubmit() {
+  console.log(`Bad Boy CLICKS`);
 }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require(`express`);
 const app = express();
 
 //const bodyParser = require(`body-parser`);
+app.use(express.static(`server/public`));
 
 app.listen(5000, () => {
   console.log(`I'M LISTENING, BABY`);
