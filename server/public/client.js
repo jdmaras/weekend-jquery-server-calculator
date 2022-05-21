872690e6e23a217920d3aa0e412bf0b030e90af1
John Maras <sportsballenthusiast@gmail.com>
2022-05-21 12:55:19 -0500

    Got input values to post to my server

--- server/public/client.js
+++ server/public/client.js
@@ -2,14 +2,31 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log(`I'm ready - Sponge Bob`);
-  $("#additionButton").on("click", onSubmit);
-  $("#subtractionButton").on("click", onSubmit);
-  $("#multiplicationButton").on("click", onSubmit);
-  $("#divideButton").on("click", onSubmit);
+  $(".mathButton").on("click", onSubmit);
   $("#clearButton").on("click", onSubmit);
   $("#equalButton").on("click", onSubmit);
 }
 
 function onSubmit() {
+  //evt.preventDefault();
   console.log(`Bad Boy CLICKS`);
+  let bothInputs = {
+    inputOne: $("#inputOne").val(),
+    inputTwo: $("#inputTwo").val(),
+  };
+  console.log(`Do I get input values?`, bothInputs.inputOne);
+  console.log(`Do I get input values?`, bothInputs.inputTwo);
+  //TEST - WORKS
+  $.ajax({
+    url: "/inputvalue",
+    method: "POST",
+    data: bothInputs,
+  })
+    .then((response) => {
+      console.log("POST IS WORKING", response);
+      // TEST - WORKS
+    })
+    .catch((error) => {
+      console.log("NOPE - FAILED - POST", error);
+    });
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 const express = require(`express`);
+const bodyParser = require(`body-parser`);
 const app = express();
 
-//const bodyParser = require(`body-parser`);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let currentMathArray = [];
+let previousMathArray = [];
+
 app.use(express.static(`server/public`));
+
+app.post("/inputvalue", (req, res) => {
+  //inputValue is url from 'post' method so you have to accept here
+  let currentInputs = req.body;
+  //console.log(`the posted input values: `, req.body);
+  console.log(`posted with new variable`, currentInputs);
+  //Test - WORKS
+  res.sendStatus(201);
+});
 
 app.listen(5000, () => {
   console.log(`I'M LISTENING, BABY`);
