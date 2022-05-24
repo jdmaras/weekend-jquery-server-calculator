09f26aca863d7a5a317ccd1bdf7ad85fe6ee3246
John Maras <sportsballenthusiast@gmail.com>
2022-05-23 19:59:20 -0500

    changed the ports

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -66,7 +67,7 @@ function divideTogetherInputs(division) {
   console.log(`This divides the two`, divideTogetherInputs);
 }
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`I'M LISTENING, BABY`);
 });
 
